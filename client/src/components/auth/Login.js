1cb0374e4f6c41c91cc8e561691d11c3b53ac09c
Charlene Kwok <charlenekwok499@gmail.com>
2022-02-08 15:09:46 +1100

    add signup form frontend

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "../header/Nav";
 import Input from "../UI/Input";
-import classes from "./Login.module.scss";
+import classes from "./form.module.scss";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import google from "../../assets/Google.svg";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Nav />
@@ -21,11 +23,12 @@ const Login = () => {
               .email("👉 Invalid email address")
               .required("👉 Please enter your Email address"),
             loginPwd: Yup.string()
-              .min(1, "👉 password cannot be empty")
+              .min(1, "👉 Password cannot be empty")
               .required("👉 Please enter your password"),
           })}
           onSubmit={(values) => {
             console.log(values);
+            navigate("/profile");
           }}
         >
           <Form className={classes.card}>
@@ -33,7 +36,7 @@ const Login = () => {
             <Input id="loginEmail" text="Email" type="email" />
             <Input id="loginPwd" text="Password" type="password" />
             <button>LOGIN</button>
-            <button>SIGNUP</button>
+            <button onClick={() => navigate("/signup")}>SIGNUP</button>
             <button className={classes.google}>
               <img src={google} alt="google login button" />
               <p>Sign in with Google</p>
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,8 +1,55 @@
 import React from "react";
-import classes from "./Signup.module.scss";
+import Nav from "../header/Nav";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import classes from "./form.module.scss";
+import { useNavigate } from "react-router-dom";
+import Input from "../UI/Input";
 
 const Signup = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <Nav />
+      <div className={classes.container}>
+        <Formik
+          initialValues={{
+            signupUsername: "",
+            signupEmail: "",
+            signUpPwd: "",
+            signUpPwd2: "",
+          }}
+          validationSchema={Yup.object({
+            signupUsername: Yup.string()
+              .min(1, "👉 Username cannot be empty")
+              .required("👉 Please enter your username"),
+            signupEmail: Yup.string()
+              .email("👉 Invalid email address")
+              .required("👉 Please enter your Email address"),
+            signUpPwd: Yup.string()
+              .min(6, "👉 Password must be longer than 6")
+              .required("👉 Please enter your password"),
+            signUpPwd2: Yup.string()
+              .oneOf([Yup.ref("signUpPwd"), null], "👉 Passwords must match")
+              .required("👉 Please enter your password again"),
+          })}
+          onSubmit={(values) => {
+            console.log(values);
+            navigate("/profile");
+          }}
+        >
+          <Form className={classes.card}>
+            <h1>Signup</h1>
+            <Input id="signupUsername" text="Username" type="text" />
+            <Input id="signupEmail" text="Email" type="email" />
+            <Input id="signUpPwd" text="Password" type="password" />
+            <Input id="signUpPwd2" text="Confirm Password" type="password" />
+            <button>SIGNUP</button>
+          </Form>
+        </Formik>
+      </div>
+    </>
+  );
 };
 
 export default Signup;
