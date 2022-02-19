ec5d7a7c0cfbb30e76b8241ac6f2ed40a715e3f7
Charlene Kwok <charlenekwok499@gmail.com>
2022-02-19 15:49:29 +1100

    update

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ MERN project
 
 Tech stack: React, Redux, Nodejs, Expressjs, Mongodb, Sass
 
-Status: Finished auth part and still building....
+Status: Finished auth and home page
--- client/src/components/space/Space.js
+++ client/src/components/space/Space.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
+import { getSaveList } from "../../api/feature-api";
 import Avatar from "../header/Avatar";
 import Button from "../UI/Button";
 import Page from "../UI/Page";
+import CollectionCard from "./CollectionCard";
 import classes from "./Space.module.scss";
 
 const Space = () => {
   const user = JSON.parse(localStorage.getItem("profile"));
-  if (!user) {
-    return <Redirect to="/login" />;
-  }
+  const [collection, setCollection] = useState([]);
+
+  useEffect(() => {
+    if (!user) {
+      return <Redirect to="/login" />;
+    }
+    const getCollection = async () => {
+      const resp = await getSaveList(user._id);
+      setCollection(resp.data.saveList);
+      console.log("🚀 ~ collection", collection);
+    };
+    getCollection();
+  }, [collection, user]);
 
   return (
     <Page className={classes["space-container"]}>
@@ -25,8 +37,19 @@ const Space = () => {
         </div>
       </div>
       <section>
-        <Button text="COLLECTION" to="/space/collection" />
-        <Button text="POSTS" to="/space/posts" />
+        <div>
+          <Button text="COLLECTION" to="/space/collection" />
+          <Button text="POSTS" to="/space/posts" />
+        </div>
+        {collection ? (
+          <div>
+            {collection.map((item, i) => (
+              <CollectionCard key={i} item={item} />
+            ))}
+          </div>
+        ) : (
+          <p>Cannot find any saved location</p>
+        )}
       </section>
     </Page>
   );
