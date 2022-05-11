d7b25730ca70b670aacf24e96756c66414d8db5c
Charlene Kwok <charlenekwok499@gmail.com>
2022-05-11 10:20:46 +1000

    remove comment

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,26 @@
-import React, { Fragment, useEffect, useState } from "react";
-import classes from "./App.module.scss";
-import open from "./assets/037.png";
+import React, { Fragment, useEffect } from "react";
 import Main from "./components/home/Main";
-import { Switch, Route, Redirect, useHistory } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Explore from "./components/explore/Explore";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
-import { useDispatch, useSelector } from "react-redux";
-import { checkLogin, userLogout } from "./store/auth-actions";
-import PwdReset from "./components/auth/PwdReset";
+import { useDispatch } from "react-redux";
 import ForgotPwd from "./components/auth/ForgotPwd";
 import NotExist from "./components/404pages/NotExist";
 import Open from "./components/openPage/Open";
 import Space from "./components/space/Space";
 import decode from "jwt-decode";
 import { authActions } from "./store/auth-slice";
-import PostPage from "./components/explore/PostPage";
 import UserSpace from "./components/space/UserSpace";
+import { userLogout } from "./store/auth-actions";
 
 function App() {
-  // const [openPage, setOpenPage] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("profile"));
     if (user) {
-      console.log("🚀 ~ user", user);
       const decodedToken = decode(user.token);
-      console.log("🚀 ~ user.token", user.token);
       if (decodedToken.exp * 1000 < new Date().getTime()) {
         localStorage.removeItem("profile");
         dispatch(userLogout(decodedToken.email));
@@ -37,21 +30,10 @@ function App() {
         dispatch(authActions.login({ user: user }));
       }
     }
-    // setTimeout(() => {
-    //   setOpenPage(false);
-    // }, 2600);
   }, []);
 
   return (
     <Fragment>
-      {/* {openPage ? (
-        <div className={classes.container}>
-          <div>
-            <img src={open} alt="opening" width="30%" />
-            <p className={classes.opening}>WeTravel</p>
-          </div>
-        </div>
-      ) : ( */}
       <Switch>
         <Route path="/" exact component={Open} />
         <Route path="/home" exact component={Main}>
